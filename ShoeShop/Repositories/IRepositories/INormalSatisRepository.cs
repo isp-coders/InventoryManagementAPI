04071b927b1c1a599b4fa8137750bfc8d92aa14b
foaudkajj <fouadalikaj@gmail.com>
2020-02-05 15:05:57 +0300

    NormalSatis - PaymentMethod Repository

---
+++ ShoeShop/Repositories/IRepositories/INormalSatisRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShoeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoeShop.Repositories.IRepositories
+{
+    public interface INormalSatisRepository
+    {
+        Product GetProductDetails(string ProductFullCode);
+    }
+}
---
+++ ShoeShop/Repositories/IRepositories/IPaymentMethodsRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoeShop.Repositories.IRepositories
+{
+    public interface IPaymentMethodsRepository
+    {
+        Task<IEnumerable<PaymentMethod>> GetPaymentMethods();
+        Task<Exception> PutPaymentMethod(int id, PaymentMethod branch);
+        Task<PaymentMethod> PostPaymentMethods(PaymentMethod[] branches);
+        Task<Exception> DeletePaymentMethod(int id);
+    }
+}
