c604fb24e34b3e015518e4708b4c3ac188bc0d2d
Muhammed Fuat Nuroğlu <fuat.nuroglu@dgpaysit.com>
2021-01-03 22:14:34 +0300

    Raporlarda Reflection ile İyileştirmeler Yapıldı

---
+++ InventoryManagement.Reporting/ReportCreators/BestSellingProductsReportCreator.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Reporting.ReportCreators
+{
+    public class BestSellingProductsReportCreator
+    {
+        public XtraReport CreateReport(XtraReport report, string url)
+        {
+            return report;
+        }
+    }
+}
---
+++ InventoryManagement.Reporting/ReportCreators/ProductLabelReportCreator.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.XtraReports.UI;
+using InventoryManagement.Reporting.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Reporting.ReportCreators
+{
+    public class ProductLabelReportCreator
+    {
+        public XtraReport CreateReport(XtraReport report, string url)
+        {
+            string paramName = url.Substring(url.IndexOf("?") + 1, url.IndexOf("=") - (url.IndexOf("?") + 1));
+            string[] paramValue = url.Substring(url.IndexOf("=") + 1).Split(",");
+            report.Parameters[paramName].Value = paramValue;
+            report.Parameters[paramName].Visible = true;
+            report.RequestParameters = false;
+            return report;
+        }
+    }
+}
